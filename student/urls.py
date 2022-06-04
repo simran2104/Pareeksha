28f4ed16206bdc0cec9063b47df9384798c9954a
Simran_Dhiman <simrandhiman5808@gmail.com>
2022-06-04 08:08:41 +0530

    Added Files from local

---
+++ student/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from student import views
+from django.contrib.auth.views import LoginView
+
+urlpatterns = [
+path('studentclick', views.studentclick_view),
+path('studentlogin', LoginView.as_view(template_name='student/studentlogin.html'),name='studentlogin'),
+path('studentsignup', views.student_signup_view,name='studentsignup'),
+path('student-dashboard', views.student_dashboard_view,name='student-dashboard'),
+path('student-exam', views.student_exam_view,name='student-exam'),
+path('take-exam/<int:pk>', views.take_exam_view,name='take-exam'),
+path('start-exam/<int:pk>', views.start_exam_view,name='start-exam'),
+
+path('calculate-marks', views.calculate_marks_view,name='calculate-marks'),
+path('view-result', views.view_result_view,name='view-result'),
+path('check-marks/<int:pk>', views.check_marks_view,name='check-marks'),
+path('student-marks', views.student_marks_view,name='student-marks'),
+path('student-faq', views.student_faq_view,name='student-faq'),
+path('student-sample', views.student_sample_view,name='student-sample'),
+path('sample1', views.student_sample1_view,name='sample1'),
+path('sample2', views.student_sample2_view,name='sample2'),
+path('student-mock', views.student_mock_view,name='student-mock'),
+]
